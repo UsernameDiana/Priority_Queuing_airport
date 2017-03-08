e2486545807095f022a9fbbbfe449552d4cc458e
UsernameDiana <ms.strele@gmail.com>
2017-03-08 14:26:39 +0100

    starting on queues implementation >>>> PersonHeap class

--- src/main/java/queues/PriorityQueue.java
+++ src/main/java/queues/PriorityQueue.java
@@ -1,8 +1,8 @@
 package queues;
 
 // Create prioraty queue, to prioratise which passengers can be removed first
+// remove the maximum and insert = prioraty queue
 
 public interface PriorityQueue<T extends Comparable<T>> extends Queue<T> {
-  // TODO
     
   }
